4940ac516182f277993696be65ef9aaf2ca14894
Tamojit <tamosrk@gmail.com>
2024-04-15 20:30:47 +0530

    created the grid

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const container = document.querySelectorAll(".container")
+const containers = document.querySelectorAll(".container");
 
-for(let i = 0; i<=16; i++){
-    container.innerHTML += '<div class="row" ></div>'
-}
-
-const row = document.querySelectorAll(".row")
-
-for(let i = 0; i<=16; i++){
-    row.innerHTML += '<div class = "column" ></div'>
-};
+containers.forEach(container => {
+    for (let i = 0; i < 16; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        
+        for (let j = 0; j < 16; j++) {
+            const column = document.createElement('div');
+            column.classList.add('column');
+            row.appendChild(column);
+        }
+    }
+});
