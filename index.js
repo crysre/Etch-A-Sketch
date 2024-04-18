5419ee4512f045094f18aff32997bb3b584347da
Tamojit <tamosrk@gmail.com>
2024-04-19 00:31:06 +0530

    Made it working

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 const containers = document.querySelectorAll(".container");
 
-containers.forEach(container => {
-    for (let i = 0; i < 16; i++) {
-        const row = document.createElement('div');
-        row.classList.add('row');
-        container.appendChild(row);
+
+let gridInput = 0
+
+function returnText(){
+    let input = document.getElementById("userInput").value;
+    
+    gridInput = input;
+
+    if(gridInput > 100){
+        alert("Please enter < 100")
+    }else{
+        containers.forEach(container => {
+            for (let i = 0; i < gridInput; i++) {
+                const row = document.createElement('div');
+                row.classList.add('row');
+                row.classList.add('div');
+                container.appendChild(row);
+            
+                
+                for (let j = 0; j < gridInput; j++) {
+                    const column = document.createElement('div');
+                    column.classList.add('column');
+                    column.classList.add('div');
+                    row.appendChild(column);
+                }
+            }
+            const sRow = document.querySelectorAll(".row");
+            sRow.forEach(row=>{
+                row.style.height = `${800 / gridInput}px`;
+            })
+        });
         
-        for (let j = 0; j < 16; j++) {
-            const column = document.createElement('div');
-            column.classList.add('column');
-            row.appendChild(column);
-        }
+        let currentColourIndex = 0;
+        const div = document.querySelectorAll(".div")
+        const colours = ["#635985", "#443C68", "#393053", "#18122B"]
+        
+        div.forEach(div => {
+            div.addEventListener("mouseover", () => {
+                const colour = colours[currentColourIndex];
+                div.style.backgroundColor = colour;
+                currentColourIndex = (currentColourIndex +1)% colours.length;
+            })
+        })
     }
-});
+    
+    }
