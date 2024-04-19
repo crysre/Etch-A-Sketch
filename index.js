680cbe123ab8086733216214e92851a119c3178b
Tamojit <tamosrk@gmail.com>
2024-04-19 20:50:53 +0530

    fixedsss

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function defaulty(){
                 const r = Math.floor(Math.random()*255);
                 const g = Math.floor(Math.random()*255);
                 const b = Math.floor(Math.random()*255);
-                div.style.backgroundColor = rgb(${r}, ${g}, $(b));
+                div.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
                 currentColourIndex = (currentColourIndex +1)% colours.length;
             })
         })
